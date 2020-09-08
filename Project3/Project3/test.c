c2b5f9f8b9ca77abae0b2745741d38c30af7f858
my-Shadows <sfedsrex@163.com>
2020-09-08 23:45:10 +0800

    9/8 study

--- Project3/Project3/test.c
+++ Project3/Project3/test.c
@@ -1,3 +1,82 @@
+//int count_one_bits(int n)
+//{
+//	int count = 0;
+//	while (n)
+//	{
+//		n = n & (n - 1);
+//		count++;
+//	}
+//	return count;
+//}
+//void main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	printf("%d", count_one_bits(n));
+//}
+//typedef struct Stu
+//{
+//	char name[20];
+//	int age[5];
+//} Stu;
+//int main()
+//{
+//	Stu s1 = { "小红",10 };
+//	printf("%s", s1.name);
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 1,2,3,4,5,6 };
+//	for (i = 0; i <= 12; i++)
+//	{
+//		printf("呵呵\n");
+//		arr[i] = 0;
+//	}
+//	system("pause");
+//	return 0;
+//}
+//void my_strcpy(char* b, char* a)//6分
+//{
+//	int i = 0;
+//	for (i = 0; *(a+i) != '\0'; i++)
+//	{
+//		*(b + i) = *(a + i);
+//	}
+//	*(b + i) = *(a + i);
+//}
+//void my_strcpy(char* a, char* b)//7分
+//{
+//	while (*a++ = *b++)
+//	{
+//		;
+//	}
+//}
+//void my_strcpy(char* a, char* b)//7.5分
+//{
+//	if (a != NULL && b != NULL)
+//	{
+//		while (*a++ = *b++)
+//		{
+//			;
+//		}
+//	}
+//	else
+//	{
+//		printf("传参错误\n");
+//	}
+//}
+#include<assert.h>
+//void my_strcpy(char* a, char* b)//8分
+//{
+//	assert(a != NULL);//断言
+//	assert(b != NULL);
+//	while (*a++ = *b++)
+//	{
+//		;
+//	}
+//}
 //#include <stdio.h>
 //int main()
 //{
@@ -10,6 +89,7 @@
 //	//内存是小端模式
 //	return 0;
 //}
+
 int* twoSum(int* nums, int numsSize, int target, int* returnSize)
 {
     int i = 0, j = 0;
