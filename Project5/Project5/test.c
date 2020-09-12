30ca86a072baf4a68e43bd83b326be7af99ba3fa
my-Shadows <sfedsrex@163.com>
2020-09-12 22:16:30 +0800

    9/11 study

---
+++ Project5/Project5/test.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<string.h>
+//int main()
+//{
+//	char a[1000] = { 1,2,3,4,5 };
+//	a[2] = 0;
+//	printf("%d", strlen(a));
+//	return 0;
+//}
+//unsigned char i = 0;
+//int main()
+//{
+//	for (i = 0; i <= 255; i++)
+//	{
+//		printf("hello world\n");
+//	}//死循环
+//
+//	return 0 ;
+//}
+//int main()
+//{
+//	double d = 1E10;
+//	printf("%lf\n", d);
+//	return 0;
+//}
+//int main()
+//{
+//	int n = 9;
+//	float* pFloat = (float*)&n;
+//	printf("n的值为%d\n", n);
+//	printf("*pFloat的值为%f\n", *pFloat);
+//	*pFloat = 9.0;
+//	printf("num的值为%d\n", n);
+//	printf("*pFloat的值为%f\n", *pFloat);
+//	return 0;
+//}
+int main()
+{
+	float f = 5.5;
+	//5.5
+	//101.1
+	//(-1)^0*1.011*2^2
+	//S=0;M=1.011;E=2   2+127=129
+	//0100 0000 1011 0000 0000 0000 0000 0000 0000
+	//0x40b00000
+	return 0;
+}
